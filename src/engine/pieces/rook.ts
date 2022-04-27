5aae6db827db63f70db0c52cd55a83988514b449
Nancy Singleton <Nancy.Singleton@softwire.com>
2022-04-27 14:35:02 +0100

    Allow rooks to move laterally

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Board from "../board";
 import Player from "../player";
+import GameSettings from '../gameSettings';
+import Square from '../square';
 
 export default class Rook extends Piece {
     constructor(player: Player) {
@@ -8,6 +10,39 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentLocation = board.findPiece(this);
+
+        let availableHorizontalMoves = this.getAvailableHorizontalMoves(currentLocation);
+        let availableVerticalMoves = this.getAvailableVerticalMoves(currentLocation);
+
+        return availableHorizontalMoves.concat(availableVerticalMoves);
+    }
+
+    getAvailableHorizontalMoves(currentLocation: Square) {
+        let availableHorizontalMoves: Square[] = [];
+
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            let newSquare = Square.at(currentLocation.row, i);
+            if (!newSquare.equals(currentLocation)) {
+                availableHorizontalMoves.push(newSquare);
+            }
+        }
+
+        return availableHorizontalMoves;
+    }
+
+    getAvailableVerticalMoves(currentLocation: Square) {
+        let availableVerticalMoves: Square[] = [];
+
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            let newSquare = Square.at(i, currentLocation.col);
+            if (!newSquare.equals(currentLocation)) {
+                availableVerticalMoves.push(newSquare);
+            }
+        }
+
+        return availableVerticalMoves;
     }
 }
+
+
