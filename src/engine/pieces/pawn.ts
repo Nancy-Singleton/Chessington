2d6605462223d2121cfab4a07621c4e21b2ce3a8
Nancy Singleton <Nancy.Singleton@softwire.com>
2022-04-27 12:35:55 +0100

    Allow pawns to move

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Board from "../board";
 import Player from "../player";
+import Square from '../square';
 
 export default class Pawn extends Piece {
     constructor(player: Player) {
@@ -8,6 +9,16 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentLocation = board.findPiece(this);
+        let availableMoves = [];
+
+        if (this.player === Player.WHITE) {
+            availableMoves.push(Square.at(currentLocation.row + 1, currentLocation.col));
+        }
+        if (this.player === Player.BLACK) {
+            availableMoves.push(Square.at(currentLocation.row - 1, currentLocation.col));
+        }
+
+        return availableMoves;
     }
 }
