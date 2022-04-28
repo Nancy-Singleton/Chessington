197d9054e64dfab738cd9928ae441c09dab226a1
Nancy Singleton <Nancy.Singleton@softwire.com>
2022-04-28 11:09:40 +0100

    Allow knights to move

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Board from "../board";
 import Player from "../player";
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Knight extends Piece {
     constructor(player: Player) {
@@ -8,6 +10,27 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentLocation = board.findPiece(this);
+        const availableMoves: Square[] = [];
+
+        availableMoves.push(Square.at(currentLocation.row + 2, currentLocation.col + 1));
+        availableMoves.push(Square.at(currentLocation.row + 2, currentLocation.col - 1));
+        availableMoves.push(Square.at(currentLocation.row - 2, currentLocation.col + 1));
+        availableMoves.push(Square.at(currentLocation.row - 2, currentLocation.col - 1));
+        availableMoves.push(Square.at(currentLocation.row + 1, currentLocation.col + 2));
+        availableMoves.push(Square.at(currentLocation.row + 1, currentLocation.col - 2));
+        availableMoves.push(Square.at(currentLocation.row - 1, currentLocation.col + 2));
+        availableMoves.push(Square.at(currentLocation.row - 1, currentLocation.col - 2));
+
+        return this.filterOutSquaresOutOfBounds(availableMoves, currentLocation);
+    }
+
+    filterOutSquaresOutOfBounds(availableMoves: Square[], currentLocation: Square) {
+        return availableMoves.filter(square => {
+            const rowInBounds = 0 <= currentLocation.row && currentLocation.row < GameSettings.BOARD_SIZE;
+            const colInBounds = 0 <= currentLocation.col && currentLocation.col < GameSettings.BOARD_SIZE;
+
+            return rowInBounds && colInBounds;
+        });
     }
 }
